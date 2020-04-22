5780435e3accff2883440240a619e8f825731cd6
Tiago Weber <tiagowebersc@gmail.com>
2020-04-22 13:33:07 +0200

    Created API project

---
+++ API/src/main/java/com/cadeodinheiro/CadêODinheiroApplication.java
@@ -0,0 +1,13 @@
+package com.cadeodinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadêODinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadêODinheiroApplication.class, args);
+	}
+
+}
---
+++ API/src/test/java/com/cadeodinheiro/CadêODinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadeodinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadêODinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
