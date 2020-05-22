e100453ebe61f58890dff6159ed6a78d90e088fd
Tiago Weber <tiagowebersc@gmail.com>
2020-05-22 08:11:22 +0200

    Implementing security with jwt

--- BackEnd/src/main/java/lu/cadeodinheiro/CadeODinheiroApplication.java
+++ BackEnd/src/main/java/lu/cadeodinheiro/CadeODinheiroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadêODinheiroApplication {
+public class CadeODinheiroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadêODinheiroApplication.class, args);
+		SpringApplication.run(CadeODinheiroApplication.class, args);
 	}
 
 }
