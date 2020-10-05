e45f89399c6c7e9b55bd792df0aa82283e71d0e4
Tiago Weber <tiagowebersc@gmail.com>
2020-10-05 20:35:04 +0200

    Chart components

--- FrontEnd/src/app/pages/pages.module.ts
+++ FrontEnd/src/app/pages/pages.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
 import { NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 
 @NgModule({
   imports: [
@@ -12,9 +15,16 @@ import { PagesRoutingModule } from './pages-routing.module';
     ThemeModule,
     NbMenuModule,
     DashboardModule,
+    NgxEchartsModule,
   ],
   declarations: [
     PagesComponent,
+    PieChartComponent,
+    BarChartComponent,
+  ],
+  exports: [
+    PieChartComponent,
+    BarChartComponent,
   ],
 })
 export class PagesModule {
